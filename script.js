6f3e2d4298e83e41606e56e4ae41f81ce15507cb
FabioBCjunior <143199420+FabioBCjunior@users.noreply.github.com>
2025-08-01 08:08:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const CaixaPrincipal = document.querySelectorAll(".caixa-principal");
+const CaixaPergunta = document.querySelectorAll(".caixa-pergunta");
+const CaixaAlternativa = document.querySelectorAll(".caixa-alternativa");
+const CaixaResultado = document.querySelectorAll(".caixa-resultado");
+const TextoResultado = document.querySelectorAll(".texto-resultado");
+
+const Perguntas = [
+    {
+         enunciado:"Pergunta 1",
+         alernativas:[
+         "Alternativa 1",
+         "Alternativa 2"
+       ]
+    },
+            {
+        enunciado:"Pergunta 2",
+        alernativas:[
+         "Alternativa 1",
+         "Alternativa 2"
+               ]
+            },
+];
