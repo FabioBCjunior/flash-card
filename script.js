fdb9c8495e305bd64f88fbb2561c6083e82fc905
FabioBCjunior <143199420+FabioBCjunior@users.noreply.github.com>
2025-08-06 08:57:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-const CaixaPrincipal = document.querySelectorAll(".caixa-principal");
-const CaixaPergunta = document.querySelectorAll(".caixa-pergunta");
-const CaixaAlternativa = document.querySelectorAll(".caixa-alternativa");
-const CaixaResultado = document.querySelectorAll(".caixa-resultado");
-const TextoResultado = document.querySelectorAll(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const Perguntas = [
+const perguntas = [
     {
-         enunciado:"Pergunta 1",
-         alernativas:[
-         "Alternativa 1",
-         "Alternativa 2"
-       ]
-    },
-            {
-        enunciado:"Pergunta 2",
-        alernativas:[
-         "Alternativa 1",
-         "Alternativa 2"
-               ]
+         enunciado:"Qual a principal função de um processador em um computador?",
+         alernativas:["Armazenar dados temporários","Executar cálculos e processar informações"]
             },
+    {
+        enunciado:"Pergunta 2",
+        alernativas:["Alternativa 1","Alternativa 2"]
+            },
+    {
+        enunciado:"Pergunta 3",
+        alernativas:["Alternativa 1","Alternativa 2"]
+            },
+    {
+        enunciado:"Pergunta 4",
+        alernativas:["Alternativa 1","Alternativa 2"]
+            },
+    {
+        enunciado:"Pergunta 5",
+        alernativas:["Alternativa 1","Alternativa 2"]
+            }
 ];
+
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta()
